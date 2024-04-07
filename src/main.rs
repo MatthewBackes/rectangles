2404bdb38f22891bab440a14949c22b549d4f22d
Strom-Cuzewon <mbackes5052@gmail.com>
2024-04-06 20:14:54 -0500

    Tested with additional functions.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self { // Associated Function
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
@@ -11,14 +32,30 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 80,
+        height: 65,
+    };
+
     dbg!(&rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    if rect1.width() {
+        println!("The width of rect1 is non-zero and is equal to {}", rect1.width);
+    }
+
+    println!("Can rect1 contain rect2? ANSWER: {}", rect1.can_hold(&rect2));
+    println!("Can rect1 contain rect3? ANSWER: {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(40);
+    println!("Square made with associated function: {} {}", sq.height, sq.width);
 }
